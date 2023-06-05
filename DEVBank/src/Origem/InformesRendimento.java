950111c936bf402c6ca611534bb4a90adf617915
Lucas Floriano da Silva <112184116+lucasflorianodev@users.noreply.github.com>
2023-06-05 18:01:23 -0300

    commit 013

--- DEVBank/src/Origem/InformesRendimento.java
+++ DEVBank/src/Origem/InformesRendimento.java
@@ -1,6 +1,55 @@
 package Origem;
 
+import static Origem.ContaCorrente.saldocorrente;
+import static Origem.ContaPoupanca.saldopoupanca;
+import static Origem.Investimentos.saldoInvestimentos;
+import static Origem.DevEconomy.saldoDevEconomy;
+
+
 public class InformesRendimento {
-    public static void exibirOpcoesInformesRendimento() {
+    public static void exibirOpcoesInformeRendimentos() {
+        System.out.println("\n===============================");
+        System.out.println("INFORME DE RENDIMENTOS");
+        System.out.println("===============================");
+        System.out.println("Bem-vindo ao sistema de solicitação de informe de rendimentos!");
+        System.out.print("Digite o seu e-mail: ");
+
+        System.out.println("\nProcessando os Dados no Sistema");
+        System.out.println("||||||||||||||");
+        System.out.println("||||||||||||||||||||||||||||");
+
+        boolean informeDisponivel = verificarDisponibilidadeInforme();
+
+        if (informeDisponivel) {
+            System.out.println("Informe de rendimentos enviado para o seu e-mail.");
+        } else {
+            System.out.println("Desculpe, o informe de rendimentos não está disponível no momento.");
+        }
+    }
+
+    private static boolean verificarDisponibilidadeInforme() {
+        double saldoContaCorrente = obterSaldoContaCorrente();
+        double saldoContaPoupanca = obterSaldoContaPoupanca();
+        double saldoInvestimentos = obterSaldoInvestimentos();
+        double saldoDevEconomy = obterSaldoDevEconomy();
+        double limiteMinimoSaldo = 28559.70;
+
+        return saldoContaCorrente + saldoContaPoupanca + saldoInvestimentos + saldoDevEconomy >= limiteMinimoSaldo;
+    }
+
+    private static double obterSaldoContaCorrente() {
+        return saldocorrente;
+    }
+
+    private static double obterSaldoContaPoupanca() {
+        return saldopoupanca;
+    }
+
+    private static double obterSaldoInvestimentos() {
+        return saldoInvestimentos;
+    }
+
+    private static double obterSaldoDevEconomy() {
+        return saldoDevEconomy;
     }
 }
