61bcbfed234fb4c7658081043b63d73a6e4bd000
Luiz <112568979+luizclaudio346@users.noreply.github.com>
2023-06-05 19:04:51 -0300

    Commit 015

--- DEVBank/src/Origem/ClienteContaDigital.java
+++ DEVBank/src/Origem/ClienteContaDigital.java
@@ -4,20 +4,12 @@ import exceptions.ContaSenhaInvalidaException;
 import exceptions.NumeroContaInvalidoException;
 import exceptions.SenhaInvalidaException;
 
-import java.text.NumberFormat;
-import java.util.Locale;
-
 public class ClienteContaDigital {
 
-    private final String numeroConta;
-    private final String senha;
-    private final String apelido = null;
-    private final String tipoCartao = null;
-    private int limiteCredito = 0;
-    private double saldoContaCorrente = 0;
-    private double saldoPoupanca = 0;
-    private double saldoInvestimentos = 0;
-    private double saldoDevEconomy = 0;
+    private final double saldoContaCorrente = 0;
+    private final double saldoContaPoupanca = 0;
+    private final double saldoInvestimentos = 0;
+    private final double saldoDevEconomy = 0;
 
     public ClienteContaDigital(String numeroConta, String senha) throws ContaSenhaInvalidaException {
         // verifica se tanto o número da conta quanto a senha são válidos
@@ -34,30 +26,6 @@ public class ClienteContaDigital {
             throw new SenhaInvalidaException("Senha inválida");
         }
 
-        this.numeroConta = numeroConta;
-        this.senha = senha;
-        this.saldoContaCorrente = saldoContaCorrente;
-        this.saldoPoupanca = saldoPoupanca;
-    }
-
-    public String getNumeroConta() {
-        return numeroConta;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public String getApelido() {
-        return apelido;
-    }
-
-    public String getTipoCartao() {
-        return tipoCartao;
-    }
-
-    public int getLimiteCredito() {
-        return limiteCredito;
     }
 
     public double getSaldoContaCorrente() {
@@ -65,7 +33,7 @@ public class ClienteContaDigital {
     }
 
     public double getSaldoPoupanca() {
-        return saldoPoupanca;
+        return saldoContaPoupanca;
     }
 
     public double getSaldoInvestimentos() {
@@ -74,16 +42,5 @@ public class ClienteContaDigital {
 
     public double getSaldoDevEconomy() {
         return saldoDevEconomy;
-    }
-
-    public String getSaldoContaCorrenteFormatado() {
-        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
-        return nf.format(saldoContaCorrente);
-    }
-
-    public void setSenha(String senha) {
-    }
-
-    public void toString(String senha) {
     }
 }
